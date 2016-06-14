93510fede43e82cf34c8db39791c6a2adda31e6c
Yaron Yagoda <yaronyagoda@Yarons-MacBook-Pro.local>
2016-06-14 14:00:23 +0300

    Adding users

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160614103232) do
+ActiveRecord::Schema.define(version: 20160614105836) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string   "name"
     t.string   "food_type"
     t.string   "speed"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
